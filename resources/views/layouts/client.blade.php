a562ecbb951c0ad6b74af2dd532f0e602e242628
huyct <huydevct@gmail.com>
2024-08-27 11:08:43 +0700

    fix: update client

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -98,7 +98,7 @@
 
             <!-- dropdown -->
             <div
-                class="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
+                class="z-10 absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
                 @foreach($categories as $category)
                     <a href="/category/{{$category->slug}}"
                        class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
