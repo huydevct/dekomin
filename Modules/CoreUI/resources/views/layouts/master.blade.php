87fedb70c29c6506d0e11261d182dc031f8d98b8
huyct <huydevct@gmail.com>
2024-08-29 16:09:58 +0700

    fix: update cms

--- Modules/CoreUI/resources/views/layouts/master.blade.php
+++ Modules/CoreUI/resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
     <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
     <meta name="author" content="Łukasz Holeczek">
     <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
-    <title>AI Math CMS</title>
+    <title>Dekomin CMS</title>
     <link rel="apple-touch-icon" sizes="57x57" href="/modules/coreui/assets/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/modules/coreui/assets/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/modules/coreui/assets/favicon/apple-icon-72x72.png">
