32489b874394ee97e8396d0d82984a8874f477b3
huy <huydevct>
2024-09-07 12:05:00 +0700

    fix: update client

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -57,13 +57,13 @@
                 </div>
             </div>
             @if(count($products) > 0)
-                <div class="grid md:grid-cols-4 grid-cols-2 gap-6">
+                <div class="grid md:grid-cols-3 grid-cols-2 gap-6">
                     @foreach($products as $product)
                         <div class="bg-white shadow rounded overflow-hidden group">
                             <div class="relative">
                                 <img
                                     src="{{$product->images[0]['url']['full'] ?? '/assets/images/products/no-image.jpg'}}"
-                                    alt="{{$product->name}}" class="w-1/2">
+                                    alt="{{$product->name}}" class="w-full">
                                 <div class="absolute inset-0 bg-black bg-opacity-40 flex items-center
                         justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
                                     <a href="/product/{{$product->slug}}"
